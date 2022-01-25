349e8da78eba9f73da15567c2dd6480ee226b456
Arthur Peruzzo <arthurkohl0@gmail.com>
2022-01-24 22:06:12 -0300

    handler exception

--- src/main/java/com/escola/escola/contato/ContatoController.java
+++ src/main/java/com/escola/escola/contato/ContatoController.java
@@ -2,12 +2,14 @@ package com.escola.escola.contato;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @Api("Contato")
 @RestController
@@ -24,6 +26,20 @@ public class ContatoController {
     @ApiOperation(value = "Inserir contato")
     public Contato inserirContato(@RequestBody @Valid Contato contato){
         return contatoService.inserirContato(contato);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationException(MethodArgumentNotValidException ex){
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fildName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fildName, errorMessage);
+        });
+
+        return errors;
     }
 
 
